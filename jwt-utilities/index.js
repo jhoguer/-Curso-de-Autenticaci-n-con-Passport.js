4e02ed93aba056b7be91bcf4f2b54f15f4962d1f
Jhon Guerrero <jhoguer@gmail.com>
2019-11-04 13:16:54 -0500

    Firmando y verificando JWT

---
+++ jwt-utilities/index.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const [, , option, secret, nameOrToken] = process.argv;
+
+if (!option || !secret || !nameOrToken) {
+  return console.log("Missing arguments");
+}
+
+function signToken(payload, secret) {
+  return jwt.sign(payload, secret);
+}
+
+function verifyToken(token, secret) {
+  return jwt.verify(token, secret);
+}
+
+console.log(option);
+if (option == "sign") {
+  console.log(signToken({ sub: nameOrToken }, secret));
+} else if (option == "verify") {
+  console.log(verifyToken(nameOrToken, secret));
+} else {
+  console.log('Option needs to be "sign" or "verify " ', option === 'verify');
+  //console.log(verifyToken(nameOrToken, secret));
+}
